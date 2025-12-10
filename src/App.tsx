9862e172c7bd4c4ce3c3def9f37ac66c5dd653c3
Shlok <shlok_mange@protonmail.com>
2025-12-11 03:15:57 +0400

    send alphaUSD test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useConnect, useConnectors, useConnection, useDisconnect } from 'wagmi'
 
 import { Hooks } from 'tempo.ts/wagmi'
+import { parseUnits } from 'viem'
 
 const App = () => {
 
@@ -11,6 +12,10 @@ const App = () => {
     const { disconnect } = useDisconnect()
 
     const { mutate: enableFaucet , isPending } = Hooks.faucet.useFundSync()
+    const { mutate: sendPayments } = Hooks.token.useTransferSync()
+
+    const recipient = ('0x0000000000000000000000000000000000000000') as `0x${string}`
+    const memo = ('') as `0x${string}`
 
     useEffect(() => {
         console.log(isConnected)
@@ -22,7 +27,6 @@ const App = () => {
             account: address!
         })
     }
-
 
     return (
         <div>
@@ -53,6 +57,20 @@ const App = () => {
             </button>
             <p>{`Don't have funds yet? Get some test funds`}</p>
             <button onClick={faucet} disabled={isPending}>Get Test Funds</button>
+            <button onClick={() => {
+                sendPayments({
+                    amount: parseUnits('100', 6),
+                    to: recipient,
+                    token: '0x20c0000000000000000000000000000000000001',
+                    memo: memo && memo.length > 0
+                        ? `0x${memo.padEnd(64, '0')}` as `0x${string}`
+                        : undefined,
+                    // Assuming the API requires 'feeToken'. Using same token as in 'token' field for now.
+                    feeToken: '0x20c0000000000000000000000000000000000001',
+                })
+            }}>
+                Great! Send 10 USD Now.
+            </button>
         </div>
     )
 }
